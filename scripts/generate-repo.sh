b5f804bf6d37fc3d3298d8ff9a4d4cf8c9456736
Twilight0 <twilight0@vivaldi.net>
2026-09-20 20:36:22 +0300

    Fix Filename path: pool/main/ not main/

--- scripts/generate-repo.sh
+++ scripts/generate-repo.sh
@@ -45,7 +45,7 @@ for arch_dir in "dists/${DIST}/${COMP}"/binary-*; do
 
         [ "$first" -eq 1 ] && first=0 || printf '\n' >> "$PKG_FILE"
 
-        printf 'Package: %s\nVersion: %s\nArchitecture: %s\nMaintainer: %s\nInstalled-Size: %s\nDepends: %s\nSection: %s\nPriority: %s\nHomepage: %s\nDescription: %s\nFilename: %s/%s\nSize: %s\nMD5sum: %s\nSHA1: %s\nSHA256: %s\n' \
+        printf 'Package: %s\nVersion: %s\nArchitecture: %s\nMaintainer: %s\nInstalled-Size: %s\nDepends: %s\nSection: %s\nPriority: %s\nHomepage: %s\nDescription: %s\nFilename: pool/%s/%s\nSize: %s\nMD5sum: %s\nSHA1: %s\nSHA256: %s\n' \
             "$pkg" "$ver" "$arch" "$maint" "$((size/1024))" "$depends" "$section" "$priority" "$homepage" "$desc" "$COMP" "$deb_name" "$size" "$md5" "$sha1" "$sha256" \
             >> "$PKG_FILE"
     done
